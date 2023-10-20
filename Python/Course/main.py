118e5a3861d148fbeba1eff8e355ae0c76d60d71
Borloo <supermaxime753@gmail.com>
2023-10-20 15:43:06 +0200

    finito

--- Python/Course/main.py
+++ Python/Course/main.py
@@ -26,7 +26,7 @@ def findTheNumber():
         tryToFind = tryToFind + 1
     print(f'Congratulation ! Try : {tryToFind}')
 
-def keepUniqueValue(datas):
+def keepUniqueValue(datas: [int]):
     finalDatas = []
     isIn = False
     for data in datas:
@@ -63,9 +63,50 @@ def moyenneNotes():
         print("Il n'y a pas de notes ...")
     else:
         moyenne = sum(notes) / len(notes)
-        print(f'La moyenne des {len(notes)} notes : {moyenne}')  
+        print(f'La moyenne des {len(notes)} notes : {moyenne}') 
 
-# findTheNumber()
-# print(keepUniqueValue([1, 5, 7, 1, 5]))
-# print(multiplicationTable())
+def caesarCipher(text: str, decallage: int):
+    dico = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26,
+    }
+    finalText = ""
+    for char in [*text.lower()]:
+        if char in dico:
+            pos = (dico[char] + decallage) % 26
+            for key, value in dico.items():
+                if value == pos:
+                    finalText += key
+        else:
+            finalText += char
+    return finalText
+
+findTheNumber()
+print(keepUniqueValue([1, 5, 7, 1, 5]))
+print(multiplicationTable())
 moyenneNotes()
+print(caesarCipher("gros caca dans mon cul !?", 3))
